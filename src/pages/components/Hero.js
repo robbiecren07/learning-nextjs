705d5e70e891bdf5b9145c544f92482138d71f86
robbiecren07 <robbiecdesigns@gmail.com>
2021-02-07 15:36:41 -0500

    tailwind full

--- src/pages/components/Hero.js
+++ src/pages/components/Hero.js
@@ -1,3 +1,16 @@
+import { Tick } from '../../icons';
+
+const outcomes = [
+    "How to build this landing page",
+    "How to create API endpoints",
+    "How to use React Hook Form and TailwindCSS",
+];
+
+const ComingSoonBage = () => (
+    <span className="bg-blue-500 text-white text-xs py-1 px-2 rounded-md mb-4 inline-block">
+        Coming Soon!
+    </span>
+);
 const Hero = () => {
     return (
     <div className="border border-gray-200 md:flex md:flex-row">
@@ -6,7 +19,22 @@ const Hero = () => {
         </div>
         <div className="px-4 py-8 md:px-8 self-center md:w-2/3">
             <h2 className="font-bold text-2xl mb-3">What you'll learn</h2>
-            <p>list item</p>
+            {outcomes && (
+                <ul className="mb-6">
+                    {outcomes.map((i => {
+                        return (
+                            <li key={i} className="text-gray-700 mb-2 flex">
+                                <span className="self-center mr-2">
+                                    <Tick className="h-4" />
+                                </span>
+                                <span className="opacity-75">{i}</span>
+                            </li>
+                        );
+                        }))
+                    }
+                </ul>
+            )}
+            <ComingSoonBage />
         </div>
     </div>
     );
